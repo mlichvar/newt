bb95cd1a170f49a3ea8deae3d964b2ddaa6f757a
ewt <ewt>
1996-03-26 04:13:48 +0000

    *** empty log message ***

--- test.c
+++ test.c
@@ -5,10 +5,13 @@
 #include "newt.h"
 
 void main(void) {
-    newtComponent b1, b2, c1, c2, r1, r2, r3, e1, e2, e3;
+    newtComponent b1, b2, r1, r2, r3, e1, e2, e3, l1, l2, l3;
+    newtComponent cs[10];
     newtComponent f, chklist;
-    char result, result2;
+    char results[10];
     char * enr1, * enr2, * enr3;
+    int i;
+    char buf[20];
 
     newtInit();
     newtCls();
@@ -21,20 +24,27 @@ void main(void) {
 
     b1 = newtButton(3, 1, "Push me");
     b2 = newtButton(18, 1, "Not me");
-    c1 = newtCheckbox(3, 6, "Check here", ' ', NULL, &result);
-    c2 = newtCheckbox(3, 7, "Not here  ", '*', NULL, &result2);
     r1 = newtRadiobutton(20, 6, "Choice 1", 0, NULL);
     r2 = newtRadiobutton(20, 7, "Choice 2", 1, r1);
     r3 = newtRadiobutton(20, 8, "Choice 3", 0, r2);
 
-    e1 = newtEntry(3, 10, "", 20, &enr1, 0);
-    e2 = newtEntry(3, 11, "Default", 20, &enr2, NEWT_ENTRY_SCROLL);
-    e3 = newtEntry(3, 12, NULL, 20, &enr3, NEWT_ENTRY_HIDDEN);
+    for (i = 0; i < 10; i++) {
+	sprintf(buf, "Check %d", i);
+	cs[i] = newtCheckbox(3, 6 + i, buf, ' ', NULL, &results[i]);
+	newtFormAddComponent(chklist, cs[i]);
+    }
 
-    newtFormAddComponents(chklist, c1, c2, NULL);
+    l1 = newtLabel(3, 10, "Fixed:");
+    l2 = newtLabel(3, 11, "Scrolls:");
+    l3 = newtLabel(3, 12, "Hidden:");
+    e1 = newtEntry(12, 10, "", 20, &enr1, 0);
+    e2 = newtEntry(12, 11, "Default", 20, &enr2, NEWT_ENTRY_SCROLL);
+    e3 = newtEntry(12, 12, NULL, 20, &enr3, NEWT_ENTRY_HIDDEN);
+
+    newtFormSetSize(chklist, 13, 3);
 
     newtFormAddComponents(f, b1, b2, chklist, NULL);
-    newtFormAddComponents(f, r1, r2, r3, e1, e2, e3, NULL);
+    newtFormAddComponents(f, r1, r2, r3, l1, l2, l3, e1, e2, e3, NULL);
 
     newtRunForm(f);
  
