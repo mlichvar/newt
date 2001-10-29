bd04625ef8d65b8efc0aad231df77db9e969f790
ewt <ewt>
2001-10-29 23:09:48 +0000

    msw! didn't allocate enough memory

--- textbox.c
+++ textbox.c
@@ -159,8 +159,8 @@ static void doReflow(const char * text, char ** resultPtr, int width,
     int kanji = 0;
 
     if (resultPtr) {
-	/* XXX I think this will work */
-	result = malloc(strlen(text) + (strlen(text) / width) + 2);
+	/* worst case is a \n after every character */
+	result = malloc((strlen(text) * 2) + 1);
 	*result = '\0';
     }
     
@@ -226,6 +226,8 @@ static void doReflow(const char * text, char ** resultPtr, int width,
 	    }
 	}
     }
+
+    result = realloc(result, strlen(result) + 1);
 
     if (badness) *badness = howbad;
     if (resultPtr) *resultPtr = result;
